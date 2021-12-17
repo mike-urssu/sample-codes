8cc32830527f3916f044461ce8d8831718e06ecc
Joon Hee Song <jhsong@vitasoft.co.kr>
2021-12-17 13:11:21 +0900

    utils: SHA256 수정

--- utils/src/encryption/SHA256.java
+++ utils/src/encryption/SHA256.java
@@ -5,21 +5,33 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SHA256 {
-    public static String encrypt(String text) throws NoSuchAlgorithmException {
-        MessageDigest md = MessageDigest.getInstance("SHA-256");
-        md.update(text.getBytes());
-        byte[] digest = md.digest();
-        return new BigInteger(1, digest).toString(16);
+    private static final String algorithm = "SHA-256";
 
-        /**
-         * 암호화 결과를 대문자로 출력하려면 toUpperCase() 메서드를 추가한다.
-         */
-//        return new BigInteger(1, digest).toString(16).toUpperCase();
+    public static String encrypt(String text) {
+        try {
+            MessageDigest md = MessageDigest.getInstance(algorithm);
+            md.update(text.getBytes());
+            byte[] digest = md.digest();
+            return new BigInteger(1, digest).toString(16);
+            /**
+             * 암호화 결과를 대문자로 출력하려면 toUpperCase() 메서드를 추가한다.
+             * return new BigInteger(1, digest).toString(16).toUpperCase();
+             */
+        } catch (NoSuchAlgorithmException e) {
+            throw new SuchAlgorithmNotExists(algorithm);
+        }
     }
 
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) {
         String text = "1234";
         System.out.println("text: " + text);
         System.out.println("encrypted text: " + encrypt(text));
+    }
+}
+
+class SuchAlgorithmNotExists extends RuntimeException {
+    public SuchAlgorithmNotExists(String algorithm) {
+        super(algorithm + " 알고리즘을 사용할 수 없습니다.\n" +
+                "[MD2, MD5, SHA-1, SHA-256, SHA-384, SHA-512] 알고리즘 중 하나를 사용하세요.");
     }
 }
