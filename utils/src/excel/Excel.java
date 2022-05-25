d35655b13b7c8f2d4095dcf447038080b88ca31c
Joon Hee Song <mike.urssu@gmail.com>
2022-05-25 17:22:50 +0900

    Feat: cellStyle 적용

--- utils/src/excel/Excel.java
+++ utils/src/excel/Excel.java
@@ -1,15 +1,24 @@
 package excel;
 
+import java.awt.Color;
 import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
+import org.apache.poi.xssf.usermodel.DefaultIndexedColorMap;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFColor;
 
 public class Excel {
     public void download() throws IOException {
@@ -19,20 +28,33 @@ public class Excel {
         ) {
             Sheet sheet = workbook.createSheet();
 
+            CellStyle greyCellStyle = workbook.createCellStyle();
+            applyCellStyle(greyCellStyle, new Color(231, 234, 236));
+
+            CellStyle blueCellStyle = workbook.createCellStyle();
+            applyCellStyle(blueCellStyle, new Color(223, 235, 246));
+
+            CellStyle bodyCellStyle = workbook.createCellStyle();
+            applyCellStyle(bodyCellStyle, new Color(255, 255, 255));
+
             // header
             int rowIndex = 0;
             Row headerRow = sheet.createRow(rowIndex++);
             Cell headerCell1 = headerRow.createCell(0);
             headerCell1.setCellValue("회사");
+            headerCell1.setCellStyle(greyCellStyle);
 
             Cell headerCell2 = headerRow.createCell(1);
             headerCell2.setCellValue("차종");
+            headerCell2.setCellStyle(greyCellStyle);
 
             Cell headerCell3 = headerRow.createCell(2);
             headerCell3.setCellValue("가격");
+            headerCell3.setCellStyle(blueCellStyle);
 
             Cell headerCell4 = headerRow.createCell(3);
             headerCell4.setCellValue("평점");
+            headerCell4.setCellStyle(blueCellStyle);
 
             // content
             List<CarExcelDto> cars = getCars();
@@ -41,15 +63,19 @@ public class Excel {
 
                 Cell bodyCell1 = bodyRow.createCell(0);
                 bodyCell1.setCellValue(car.getCompany());
+                bodyCell1.setCellStyle(bodyCellStyle);
 
                 Cell bodyCell2 = bodyRow.createCell(1);
                 bodyCell2.setCellValue(car.getName());
+                bodyCell2.setCellStyle(bodyCellStyle);
 
                 Cell bodyCell3 = bodyRow.createCell(2);
                 bodyCell3.setCellValue(car.getPrice());
+                bodyCell3.setCellStyle(bodyCellStyle);
 
                 Cell bodyCell4 = bodyRow.createCell(3);
                 bodyCell4.setCellValue(car.getRating());
+                bodyCell4.setCellStyle(bodyCellStyle);
             }
             workbook.write(outputStream);
         }
@@ -61,6 +87,18 @@ public class Excel {
             new CarExcelDto("르노삼성", "QM6", 200, 4.9),
             new CarExcelDto("기아", "K7", 300, 4.9)
         );
+    }
+
+    private void applyCellStyle(CellStyle cellStyle, Color color) {
+        XSSFCellStyle xssfCellStyle = (XSSFCellStyle) cellStyle;
+        xssfCellStyle.setFillForegroundColor(new XSSFColor(color, new DefaultIndexedColorMap()));
+        cellStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        cellStyle.setAlignment(HorizontalAlignment.CENTER);
+        cellStyle.setVerticalAlignment(VerticalAlignment.CENTER);
+        cellStyle.setBorderLeft(BorderStyle.THIN);
+        cellStyle.setBorderTop(BorderStyle.THIN);
+        cellStyle.setBorderRight(BorderStyle.THIN);
+        cellStyle.setBorderBottom(BorderStyle.THIN);
     }
 
     public static void main(String[] args) throws IOException {
